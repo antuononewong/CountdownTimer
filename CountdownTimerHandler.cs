6748885a715c8645e8ff5d217e5a48b0a2bf9e85
Anthony <48633290+antuononewong@users.noreply.github.com>
2020-11-01 21:46:48 -0600

    Add files via upload
    Initial commit - implementation of a UI countdown, based in seconds elapsed

---
+++ CountdownTimerHandler.cs
@@ -0,0 +1,42 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class CountdownTimerHandler : MonoBehaviour
+{
+    // Dependancies
+    public GameObject endMenu;
+
+    // Components
+    private TMP_Text _countdownText;
+
+    // Countdown
+    private float _timer;
+    private float _maxTimer = 120f;
+
+    private void Awake()
+    {
+        _countdownText = gameObject.GetComponent<TMP_Text>();
+        _timer = _maxTimer;
+    }
+
+    private void Update()
+    {
+        Countdown();
+        CheckGameState();
+    }
+
+    private void Countdown()
+    {
+        _timer = Mathf.Clamp(_timer - (1 * Time.deltaTime), 0f, _maxTimer);
+        _countdownText.text = _timer.ToString("#");
+    }
+
+    private void CheckGameState()
+    {
+        if (_timer <= 0)
+        {
+            Time.timeScale = 0f;
+            endMenu.SetActive(true);
+        }
+    }
+}
